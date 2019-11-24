aee58c510a28c3ca3283dcb9843edd9a4fd16d7e
Keir Whitlock <keir@whitlock.it>
2019-11-24 19:58:41 +0000

    initial home handler func

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/kwhitlock/lets-go-book
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// define a home handler func which writes a byte
+// slice as the resp body
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func main() {
+	// init a new servemux and register the home func handler for "/"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// start a new web server
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
