acf033852081f972048e60e1dbfc07834ed600d4
Keir Whitlock <keir@whitlock.it>
2019-12-01 14:36:56 +0000

    Using http.FileServer to serve static content and bolting css and other static files to base template.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,10 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
