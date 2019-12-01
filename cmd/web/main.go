f4007026cf0af4b7a1b9f3910d331e40521b44f9
Keir Whitlock <keir@whitlock.it>
2019-12-01 22:31:49 +0000

    Adding cmdline flags for server listen address and location of static content.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	static_dir := flag.String("static", "./ui/static/", "Directory where static files are located.")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
+	fileserver := http.FileServer(http.Dir(*static_dir))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
