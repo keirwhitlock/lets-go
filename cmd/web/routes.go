dc56b342afa3aadf8be2daf0f2f41a8a608026ff
Keir Whitlock <keir@whitlock.it>
2019-12-08 15:20:45 +0000

    Adding routes and handler placeholders for user authentication links.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,6 +20,12 @@ func (app *application) routes(staticDir string) http.Handler {
 	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
 	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+
 	fileserver := http.FileServer(http.Dir(staticDir))
 	mux.Get("/static/", http.StripPrefix("/static", fileserver))
 
