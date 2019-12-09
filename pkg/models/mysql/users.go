497e550888393225236eb98c04df12834c3d38a4
Keir Whitlock <keir@whitlock.it>
2019-12-09 07:42:48 +0000

    Signup form now adds user to database, and checks for dupe emails.

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -2,7 +2,11 @@ package mysql
 
 import (
 	"database/sql"
+	"strings"
 
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/go-sql-driver/mysql"
 	"github.com/kwhitlock/lets-go-book/pkg/models"
 )
 
@@ -11,7 +15,23 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
-	return nil
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+	}
+	return err
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
